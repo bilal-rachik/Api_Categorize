b661c8267df60ad0c990857abe30be01addfa231
rachikbilal <brachik@bigapps.fr>
2019-08-05 12:49:45 +0200

    vrsion 0.1

--- api_train.py
+++ api_train.py
@@ -10,7 +10,12 @@ from io import StringIO
 
 app = flask.Flask(__name__)
 
-@app.route('/api/categorize', methods=['POST'])
+@click.command("train")
+@click.option("--port", "-p", default=5000, help="Server port. [default: 5000]")
+@click.option("--host", "-h", default="0.0.0.0", help="Server host. [default: 0.0.0.0]")
+
+
+@app.route('/api/train', methods=['POST'])
 def API():  # pylint: disable=unused-variable
     if flask.request.content_type == 'text/csv':
         data = request.data.decode('utf-8')
@@ -33,6 +38,8 @@ def API():  # pylint: disable=unused-variable
     if str(flask.request.content_type).__contains__("application/x-www-form-urlencoded"):
         data = request.form['json_data']
         df = pd.DataFrame(eval(data), index=[0])
+
+
         
     result = df.to_json()
     return flask.Response(response=result, status=200, mimetype='application/json')
